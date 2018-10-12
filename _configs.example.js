6372458012b537796ed662b9da8134cb1b3ddfb4
João <jfelipesilva@gmail.com>
2018-10-11 22:34:11 -0300

    first-commit

---
+++ _configs.example.js
@@ -0,0 +1,18 @@
+//copy this file to "_config.js" and configure it with your environment needs
+
+var configs = {
+	"APP_DIR": "/home/ubuntu/trader/",
+
+    "BITFINEX_APIKEY":"",
+    "BITFINEX_APISECRET":"",
+    "BITFINEX_BASEURL":"https://api.bitfinex.com",
+
+    "WSS_PORT": "8002",
+
+    "BD_HOST":"127.0.0.1",
+    "BD_USER":"root",
+    "BD_PASSWORD":"000000",
+    "BD_DATABASE":"auto-trader"
+}
+
+module.exports = configs;
